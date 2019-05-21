959ccd40ee1cecd3d732c299eb86ed0c3cd23a92
slickerdrips <camirubin@hotmail.com>
2019-05-21 16:51:36 -0300

    Desafío clase 4

--- Clase 04/Alumno/Desafío/js/index.js
+++ Clase 04/Alumno/Desafío/js/index.js
@@ -1,13 +1,55 @@
-// Objetos:
+var studentsList = [
+  {
+    firstName: 'Juan',
+    lastName: 'Pérez',
+    dni: 45678987
+  },
+  {
+    firstName: 'Ana',
+    lastName: 'Fernandez',
+    dni: 45678989
+  },
+  {
+    firstName: 'Pedro',
+    lastName: 'Mármol',
+    dni: 45678956
+  },
+  {
+    firstName: 'Pablo',
+    lastName: 'Picapiedras',
+    dni: 45678983
+  }
+]
 
-var student = {
-  firstName: 'Marcela',
-  lastName: 'Correa',
-  age: 29
+@param {string} text
+@param {Array} studentsList
+@returns {number}
+
+function searchStudent (studentsList, text) {
+
+  var index = -1
+
+  for (var i; i < studentsList.length; i++) {
+
+  var students = studentsList[i]
+
+    if (students.firstName === text || students.lastName === text) {
+
+     index = i
+     break
+    }
+  }
+
+  return index
 }
 
-console.log(student.age)
+var text = prompt('Ingrese el nombre o apellido del estudiante que está buscando')
 
+var index = searchStudent(studentsList, text)
 
+if (index !== -1) {
 
-
+  console.log('se encontró al estudiante en la posición ' + index)
+}else {
+  console.log('No se ha podido encontrar al estudiante')
+}
