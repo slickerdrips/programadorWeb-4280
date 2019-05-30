54b89c5fca669bd3037686027cb3c731d4af59f1
slickerdrips <camirubin@hotmail.com>
2019-05-30 20:34:29 -0300

    ejercicio clase 15

---
+++ Clase 15/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  var url = 'https://swapi.co/api/people/5/'
+
+  getData(url, callback)
+
+  function getData(url, cbk) {
+    $.ajax(url)
+      .done(function(data) {
+        cbk(null, data)
+      })
+      .fail(function(error) {
+        cbk(error)
+      })
+  }
+
+  function callback(error, data) {
+    if (error) {
+      console.log('Hubo un error', error)
+    } else {
+      console.log('Todo ok', data)
+    }
+  }
+})
