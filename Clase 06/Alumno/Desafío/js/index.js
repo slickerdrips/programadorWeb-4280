8e664e473d3dcac91877bf03a83636db5964b561
slickerdrips <camirubin@hotmail.com>
2019-05-30 17:38:43 -0300

    Desafío clase 6

---
+++ Clase 06/Alumno/Desafío/js/index.js
@@ -0,0 +1,28 @@
+function includesText(text, baseText) {
+  if (typeof text === 'string' && typeof baseText === 'string') {
+    //paso las variables a upper case para que pueda encontrar el text sin problemas en el basetext
+
+    var textUpperCase = text.toUpperCase()
+    var baseTextUpperCase = baseText.toUpperCase()
+
+    if (baseTextUpperCase.indexOf(textUpperCase) !== -1) {
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
+}
+
+
+console.log(includesText('PR', 'Princess Carolyn'))
+
+console.log(includesText('ca', 'Princess Carolyn'))
+
+console.log(includesText('Princess Carolyn', 'Pr'))
+
+console.log(includesText(55, 'Pr'))
+
+
+// como puedo hacer para que además de decirme si es true or false me lo devuelva en una oración? intenté poner un console log pero se me rompe la función
